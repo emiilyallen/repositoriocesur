89ef0f7ebf1b9c654a74b861558dbb656da742af
Usuaria <Usuaria@EMILYALLEN-05>
2024-03-09 01:59:35 +0100

    Suma de los números

---
+++ EntornosDeDesarrollo5/src/tareaNoEvaluable/calculoSencillo.java
@@ -0,0 +1,18 @@
+package tareaNoEvaluable;
+
+public class calculoSencillo {
+
+	public static void main(String[] args) {
+
+		int num1,num2,num3,resultado;
+		
+		num1 = 4;
+		num2 = 12;
+		num3 = 7;
+
+		resultado = num1+num2+num3;
+		
+		System.out.println("La suma de los números 4, 12 y 7 es: "+resultado);
+	}
+
+}
