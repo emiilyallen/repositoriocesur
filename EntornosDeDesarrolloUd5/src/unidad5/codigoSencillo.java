0adfd54e6b9c27d21712e20c708d5d237d72f9b8
Usuaria <Usuaria@EMILYALLEN-05>
2024-03-09 01:34:59 +0100

    Ejemplo de Repositorios

---
+++ EntornosDeDesarrolloUd5/src/unidad5/codigoSencillo.java
@@ -0,0 +1,15 @@
+package unidad5;
+
+public class codigoSencillo {
+
+	public static void main(String[] args) {
+
+		int num1=17,num2=34,num3=51,media;
+		
+		media=(num1+num2+num3)/3;
+		
+		System.out.println("La media de 17, 34 y 51 es: " +media);
+		
+	}
+
+}
